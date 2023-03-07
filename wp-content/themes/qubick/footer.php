9c35e37f18a7e61333f135ea8c0a030741195bcb
ZieCodeProject <119549644+ZieCodeProject@users.noreply.github.com>
2023-03-07 14:13:40 +0700

    ubah ukuran font footer

--- wp-content/themes/qubick/footer.php
+++ wp-content/themes/qubick/footer.php
@@ -1,38 +1,38 @@
 
-<footer class="bg-gray-300 dark:bg-gray-300 px-24">
+<footer class="bg-gray-300 dark:bg-gray-300 px-60">
     <div class="grid grid-cols-2 gap-8 px-6 py-4 md:grid-cols-4">
     <img class="w-40" src="<?php bloginfo('template_directory');?>/images/footer/AssetLogo12.png" alt="logo">
     </div>
     <div class="grid grid-cols-2 gap-8 px-6 py-2 md:grid-cols-4">
         <div>
-            <h2 class="mb-2 text-sm font-semibold text-black uppercase dark:text-gray-400">Head Office - Surabaya, Indonesia</h2>
+            <h2 class="mb-2 text-xs font-semibold text-black uppercase dark:text-gray-400">Head Office - Surabaya, Indonesia</h2>
             <ul class="text-gray-500 dark:text-gray-400">
                 <p class="-mt-4">-</p>
                 <li class="mb-8">
-                    <a href="#" class="flex justify-start text-black hover:underline text-sm font-sans">Jl. Kalianget No.15B Perak Utara, Surabaya, Jawa Timur 60615 Ph.+62333287604</a>
+                    <a href="#" class="flex justify-start text-black hover:underline text-xs font-sans">Jl. Kalianget No.15B Perak Utara, Surabaya, Jawa Timur 60615 Ph.+62333287604</a>
                 </li>
             </ul>
         </div>
         <div>
-            <h2 class="mb-2 text-sm font-semibold text-black uppercase dark:text-gray-400">Operational Office - Medan, Indonesia</h2>
+            <h2 class="mb-2 text-xs font-semibold text-black uppercase dark:text-gray-400">Operational Office - Medan, Indonesia</h2>
             <ul class="text-gray-500 dark:text-gray-400">
                 <p class="-mt-4">-</p>
                 <li class="mb-8">
-                    <a href="#" class="flex justify-start text-black hover:underline text-sm font-sans">Graha Harmoni 5th Floor, Jl Gaharu no.28, Medan Sumatera Utara 20235 Ph.+62615141755</a>
+                    <a href="#" class="flex justify-start text-black hover:underline text-xs font-sans">Graha Harmoni 5th Floor, Jl Gaharu no.28, Medan Sumatera Utara 20235 Ph.+62615141755</a>
                 </li>
             </ul>
         </div>
         <div>
-            <h2 class="mb-2 text-sm font-semibold text-black uppercase dark:text-gray-400">Operational Office - Ha Noi, Vietnam</h2>
+            <h2 class="mb-2 text-xs font-semibold text-black uppercase dark:text-gray-400">Operational Office - Ha Noi, Vietnam</h2>
             <ul class="text-gray-500 dark:text-gray-400">
                 <p class="-mt-4">-</p>
                 <li class="mb-8">
-                    <a href="#" class="flex justify-start text-black hover:underline text-sm font-sans">18444 Due Giang Street, Long Beach District, Hanoi, Vietnam Ph.+843957711954</a>
+                    <a href="#" class="flex justify-start text-black hover:underline text-xs font-sans">18444 Due Giang Street, Long Beach District, Hanoi, Vietnam Ph.+843957711954</a>
                 </li>
             </ul>
         </div>
         <div>
-            <h2 class="mb-2 text-sm font-semibold text-black uppercase dark:text-gray-400">Reach Us</h2>
+            <h2 class="mb-2 text-xs font-semibold text-black uppercase dark:text-gray-400">Reach Us</h2>
         <div class="container pt-1">
         <div class="mb-6 flex justify-start">
       <a
@@ -72,7 +72,7 @@
       </a>
       
     </div>
-    <p class="mb-4 underline underline-offset-4 text-sm">
+    <p class="mb-4 underline underline-offset-4 text-xs">
         2022 DCM Logistics website design by Branding & Project
     </p>
         </div>

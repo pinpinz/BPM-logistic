278b44cf28446c97bc65f6726a22570cef5207c1
ZieCodeProject <119549644+ZieCodeProject@users.noreply.github.com>
2023-03-07 14:10:06 +0700

    update dhany

--- wp-content/themes/qubick/front-page.php
+++ wp-content/themes/qubick/front-page.php
@@ -3,24 +3,79 @@
 ?>
 <section class="bg-white dark:bg-gray-100">
         <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
-            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                <img class="px-11" src="<?php bloginfo('template_directory');?>/images/AssetHome1.png" alt="HeroImage">
-                <!-- <img src="./images/hero.png" alt="hero image"> -->
+            <div class="hidden lg:mt-0 lg:col-span-4 lg:flex">
+                <img class="pl-24" src="<?php bloginfo('template_directory');?>/images/AssetHome1.png" alt="HeroImage">              
             </div>
-            <div class="mr-auto place-self-center lg:col-span-7 px-24 ">
-                <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-gray-900">Connecting You, <br>Connecting Your Business</h1>
-                <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">This free and open-source landing page template was built using the utility classes from <a href="https://tailwindcss.com" class="hover:underline">Tailwind CSS</a> and based on the components from the <a href="https://flowbite.com/docs/getting-started/introduction/" class="hover:underline">Flowbite Library</a> and the <a href="https://flowbite.com/blocks/" class="hover:underline">Blocks System</a>.</p>
+            <div class="lg:mr-auto place-self-center lg:col-span-7 px-24 ">
+                <h1 class="max-w-2xl mb-4 text-2xl font-extrabold leading-none tracking-tight md:text-4xl xl:text-4xl text-yellow-300 font-sans">Connecting You, <br>Connecting Your Business</h1>
+                <p class="max-w-2xl mb-6 text-gray-700 lg:mb-8 md:text-sm lg:text-sm dark:text-gray-700 font-bold">"Creating a network to assist you in reachingyour potential buyers and growing your business." Some of our cliet have gained the benefit of greater exposure in the region, getting new prospects, and even expanding their business sector.</p>
                 <div class="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-                    <a href="https://github.com/themesberg/landwind" class="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                        <svg class="w-4 h-4 mr-2 text-gray-500 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg> View on GitHub
+                    <a href="#" class="inline-flex items-center justify-center w-full px-5 py-3 text-sm bg-yellow-400 font-medium text-center text-gray-900 hover:text-white border border-gray-200 rounded-lg sm:w-auto hover:bg-sky-900 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-sky-900 dark:focus:ring-gray-800">
+                        Request for quote
                     </a> 
-                    <a href="https://www.figma.com/community/file/1125744163617429490" class="inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                        <svg class="w-4 h-4 mr-2" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 300" width="1667" height="2500"><style type="text/css">.st0{fill:#0acf83}.st1{fill:#a259ff}.st2{fill:#f24e1e}.st3{fill:#ff7262}.st4{fill:#1abcfe}</style><title>Figma.logo</title><desc>Created using Figma</desc><path id="path0_fill" class="st0" d="M50 300c27.6 0 50-22.4 50-50v-50H50c-27.6 0-50 22.4-50 50s22.4 50 50 50z"/><path id="path1_fill" class="st1" d="M0 150c0-27.6 22.4-50 50-50h50v100H50c-27.6 0-50-22.4-50-50z"/><path id="path1_fill_1_" class="st2" d="M0 50C0 22.4 22.4 0 50 0h50v100H50C22.4 100 0 77.6 0 50z"/><path id="path2_fill" class="st3" d="M100 0h50c27.6 0 50 22.4 50 50s-22.4 50-50 50h-50V0z"/><path id="path3_fill" class="st4" d="M200 150c0 27.6-22.4 50-50 50s-50-22.4-50-50 22.4-50 50-50 50 22.4 50 50z"/></svg> Get Figma file
-                    </a>
+                    
                 </div>
             </div>
                            
         </div>
+    </section>
+
+    <section class="bg-white dark:bg-gray-100">
+    <div class="mt-0 flex lg:h-[32rem] h-[10rem] bg-[url('<?php bloginfo('template_directory');?>/images/Home.jpg')] bg-top bg-cover "></div>
+    </section>
+
+    <section class="bg-white dark:bg-gray-100">
+       
+         <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+            <div class="hidden lg:mt-0 lg:col-span-4 lg:block pl-24">
+                
+                    <h1 class="max-w-2xl mb-4 text-2xl font-bold leading-none tracking-tight md:text-4xl xl:text-4xl text-yellow-300 font-sans">MORE THAN <br> <span class="text-sky-900">25 YEARS OF</span> <br> EXPERIENCE <br></h1>  
+             
+                    <img class="" src="<?php bloginfo('template_directory');?>/images/Asset 2.png" alt="HeroImage">  
+                    
+            </div>
+            
+            <div class="lg:mr-auto place-self-center lg:col-span-8 px-24 ">
+                <p class="max-w-2xl mb-6 text-gray-700 lg:mb-8 md:text-sm lg:text-sm dark:text-gray-700 font-bold">Has been engaged in logistic for more than 25 years, now we provide better service <br>by creating a total solution by connecting and providing solution ton their business.<br>not just logistics while prioriting a long term relationship to grow.</p>
+                <h1 class="max-w-2xl mb-4 text-2xl font-bold leading-none tracking-tight md:text-4xl xl:text-4xl text-yellow-300 font-sans">1997</h1>
+                <p class="max-w-2xl mb-6 text-gray-700 lg:mb-8 md:text-sm lg:text-sm dark:text-gray-700 font-bold">The establishment of PT.Antar Benua Cahaya Custom Brokers.</p>
+                <h1 class="max-w-2xl mb-4 text-2xl font-bold leading-none tracking-tight md:text-4xl xl:text-4xl text-yellow-300 font-sans">2012</h1>
+                <p class="max-w-2xl mb-6 text-gray-700 lg:mb-8 md:text-sm lg:text-sm dark:text-gray-700 font-bold">Customer award by PT.Terminal Peti Kemas Surabaya.</p>
+                <h1 class="max-w-2xl mb-4 text-2xl font-bold leading-none tracking-tight md:text-4xl xl:text-4xl text-yellow-300 font-sans">2019</h1>
+                <p class="max-w-2xl mb-6 text-gray-700 lg:mb-8 md:text-sm lg:text-sm dark:text-gray-700 font-bold">The establishment od subsidiary PT.Benua Cahaya Mandiri <br>at Indonesia & Vietname.</p>
+                <div class="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+                    <a href="#" class="inline-flex items-center justify-center w-full px-5 py-3 text-sm bg-yellow-400 font-medium text-center text-gray-900 hover:text-white border border-gray-200 rounded-lg sm:w-auto hover:bg-sky-900 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-sky-900 dark:focus:ring-gray-800">
+                        More
+                    </a> 
+                    
+                </div>
+            </div>
+                           
+        </div>
+    </section>
+
+    <section class="bg-yellow-400 dark:bg-yellow-400">    
+        
+    <div class="grid max-w-screen-xl px-4 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-4 lg:grid-cols-12 ">
+        <div class="place-self-center lg:col-span-12 ">
+            <h1 class="max-w-2xl mb-4 text-2xl font-normal leading-none tracking-tight md:text-4xl xl:text-4xl text-white font-sans">A WIDE RANGE OF TAILORED SERVICES</h1>
+        </div>
+    </div>
+    <div class="grid max-w-screen-xl px-4 mx-auto lg:gap-8 xl:gap-0 lg:py-8 lg:grid-cols-12 grid-cols-3">
+        <div class=" place-self-center lg:col-span-4 ">
+            <img class="h-32 lg:h-auto mb-3 lg:mb-0" src="<?php bloginfo('template_directory');?>/images/Asset 4.png" alt="HeroImage"> 
+            <h2 class="hidden lg:block max-w-2xl mb-4 text-2xl font-normal leading-none tracking-tight md:text-2xl xl:text-2xl text-white font-sans text-center mt-10 pr-7">Sea Freight</h2>
+        </div>
+           <div class="flex place-self-center lg:col-span-4 lg:block">
+            <img class="h-32 lg:h-auto mb-3 lg:mb-0" src="<?php bloginfo('template_directory');?>/images/Asset 5.png" alt="HeroImage"> 
+            <h2 class="hidden lg:block max-w-2xl mb-4 text-2xl font-normal leading-none tracking-tight md:text-2xl xl:text-2xl text-white font-sans text-center mt-10 pr-7">Air Freight</h2>
+        </div>
+            <div class="flex place-self-center lg:col-span-4 lg:block">
+            <img class="h-32 lg:h-auto mb-3 lg:mb-0" src="<?php bloginfo('template_directory');?>/images/Asset 6.png" alt="HeroImage"> 
+            <h2 class="hidden lg:block max-w-2xl mb-4 text-2xl font-normal leading-none tracking-tight md:text-2xl xl:text-2xl text-white font-sans text-center mt-10 pr-7">Warehousing</h2>
+        </div>
+    </div>
+    
     </section>
 
 <?php 
--- wp-content/themes/qubick/navbar.php
+++ wp-content/themes/qubick/navbar.php
@@ -1,8 +1,8 @@
 <header id="header-menu">
 
-<nav class="bg-blue-700 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-blue-700">
+<nav class="bg-sky-900 border-gray-200 px-2 sm:px-4 py-2.5 ">
   <div class="container flex items-center justify-between mx-auto">
-    <a href="#" class="flex items-center">
+    <a href="#" class="flex items-center lg:pl-52">
         <img class="w-40" src="<?php bloginfo('template_directory');?>/images/Asset11.png" alt="logo">
         <!-- <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span> -->
     </a>
@@ -11,18 +11,18 @@
       <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
     </button>
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-blue-700 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-blue-700 dark:bg-blue-700 md:dark:bg-blue-700 dark:border-gray-700">
+      <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-sky-900 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:border-gray-700">
         <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-white-700 md:p-0 dark:text-white" aria-current="page">Who We Are?</a>
+          <a href="#" class="block py-2 pl-3 pr-4 text-white rounded hover:text-yellow-400 md:hover:bg-transparent md:border-0 md:hover:text-yellow-400 md:p-0 dark:text-gray-400 md:dark:hover:text-yellow-400 dark:hover:bg-gray-700 dark:hover:text-yellow-400 md:dark:hover:bg-transparent" aria-current="page">Who We Are?</a>
         </li>
         <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-white md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Service</a>
+          <a href="#" class="block py-2 pl-3 pr-4 text-white rounded hover:text-yellow-400 md:hover:bg-transparent md:border-0 md:hover:text-yellow-400 md:p-0 dark:text-gray-400 md:dark:hover:text-yellow-400 dark:hover:bg-gray-700 dark:hover:text-yellow-400 md:dark:hover:bg-transparent">Service</a>
         </li>
         <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-white md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+          <a href="#" class="block py-2 pl-3 pr-4 text-white rounded hover:text-yellow-400 md:hover:bg-transparent md:border-0 md:hover:text-yellow-400 md:p-0 dark:text-gray-400 md:dark:hover:text-yellow-400 dark:hover:bg-gray-700 dark:hover:text-yellow-400 md:dark:hover:bg-transparent">Contact</a>
         </li>
         <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-white md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">(62) 812 345 6789</a>
+          <a href="#" class="block py-2 pl-3 pr-4 text-white rounded hover:text-yellow-400 md:hover:bg-transparent md:border-0 md:hover:text-yellow-400 md:p-0 dark:text-gray-400 md:dark:hover:text-yellow-400 dark:hover:bg-gray-700 dark:hover:text-yellow-400 md:dark:hover:bg-transparent">(62) 812 345 6789</a>
         </li>
       </ul>
     </div>

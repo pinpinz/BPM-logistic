6b87861295957e13792d4f7cc447a1c724aa25d0
ZieCodeProject <119549644+ZieCodeProject@users.noreply.github.com>
2023-05-24 09:51:57 +0700

    revisi 6

--- wp-content/themes/qubick/page-service.php
+++ wp-content/themes/qubick/page-service.php
@@ -3,7 +3,7 @@
 ?>
 
 <main class="mt-20">
-<section class="bg-white ">  
+<!-- <section class="bg-white ">  
                         
                         <div class="grid max-w-screen-xl px-8 md:px-0 pb-4 mx-auto lg:gap-8 xl:gap-0 lg:grid-cols-12 ">
                             <div class="place-self-left lg:col-span-12 ">
@@ -72,27 +72,27 @@
                          
                             
                         </div>
-                        
-                    </section>
+                        [#fdce28] kuning #003b99 biru
+                    </section> -->
 
 
-                <section class="bg-[#fdce28]" id="service">    
+                <section class="bg-white" id="service">    
                         
                     <div class="grid max-w-screen-xl px-8 mx-auto lg:gap-8 xl:gap-0 lg:pt-4 pt-10 lg:grid-cols-12 ">
                         <div class="place-self-start lg:col-span-12 ">
-                            <h1 class="max-w-2xl mb-4 text-2xl md:text-4xl xl:text-4xl font-bold leading-none tracking-tight text-white ">OUR SERVICE</h1>
-                            <h1 class="max-w-2xl mb-4 text-gray-700 text-sm font-normal leading-none tracking-tight">We deliver all of ypur logistic needs and tailor our services and solution per your <br> requirements. We are experienced in various industries and work to cater</h1>
+                            <h1 class="max-w-2xl mb-4 text-2xl md:text-4xl xl:text-4xl font-extrabold leading-none tracking-tight text-[#fdce28] ">OUR SERVICE</h1>
+                            <h1 class="max-w-2xl mb-4 text-black text-sm font-normal leading-none tracking-tight">At our company, we understand that every shipment is unique, and that’s why we offer customizable solutions to meet your specific needs. We take pride in providing exceptional customer service to exceed your expectations. Get in touch with us today to discuss your shipping requirements, and let us take care of the rest!</h1>
                         </div>
                     </div>
 
                     <div class="grid max-w-screen-xl px-4 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:pb-16 lg:pt-10 md:grid-cols-12 grid-cols-6">
                             <div class="md:flex col-span-6 md:col-span-4 gs_revealY">
-                                <img class="md:pl-24 py-10 md:py-0" src="<?php bloginfo('template_directory');?>/images/service/Asset 9.png" alt="HeroImage">              
+                                <img class="md:pl-24 py-10 md:py-0" src="<?php bloginfo('template_directory');?>/images/revisi/Asset 5@4x.png" alt="HeroImage">              
                             </div>
                             <div class="lg:mr-auto place-self-center col-span-6 md:col-span-7 md:px-24 px-4">
-                                <h1 class="max-w-2xl mb-4 text-2xl md:text-4xl xl:text-4xl font-bold leading-none tracking-tight text-white ">SEA FREIGHT</h1>
-                                <p class="max-w-2xl mb-6 text-gray-700 lg:mb-8 text-sm  font-normal">
-                                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. In porta est sit amet condimentum ullamcorper. Pellentesque viverra pretium odio, fringilla aliquam elit bibendum in. Nulla pretium dolor id enim molestie vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus metus ut cursus eleifend. Praesent justo purus, tincidunt ut sem ut, accumsan cursus sem.
+                                <h1 class="max-w-2xl mb-4 text-2xl md:text-4xl xl:text-4xl font-extrabold leading-none tracking-tight text-[#fdce28] ">SEA FREIGHT</h1>
+                                <p class="max-w-2xl mb-6 text-black lg:mb-8 text-sm  font-normal">
+                                   Our sea freight services are designed to offer you maximum flexibility, reliability and affordability. With our extensive network of carriers, we offer a comprehensive range of sea freight options to choose from, including full container load (FCL) and less than container load (LCL) services. Our sea freight team is highly experienced and can assist you with every aspect of your sea freight requirements, from customs clearance to delivery to your door.
                                 </p>
                                 
                             </div>                                       
@@ -101,21 +101,21 @@
                     
                 </section>
 
-                <section class="bg-white" id="2">                 
+                <section class="bg-[#fdce28]" id="2">                 
                     <div class="grid max-w-screen-xl px-4 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:pb-16 pt-10 md:grid-cols-12 grid-cols-6">              
                             <div class="lg:mr-auto place-self-center col-span-6 md:col-span-7 md:px-24 px-4">
-                                <h1 class="max-w-2xl mb-4 text-2xl md:text-4xl xl:text-4xl font-bold leading-none tracking-tight text-[#fdce28] ">AIR FREIGHT</h1>
-                                <p class="max-w-2xl mb-6 text-gray-700 lg:mb-8 text-sm  font-normal">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In porta est sit amet condimentum ullamcorper. Pellentesque viverra pretium odio, fringilla aliquam elit bibendum in. Nulla pretium dolor id enim molestie vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus metus ut cursus eleifend. Praesent justo purus, tincidunt ut sem ut, accumsan cursus sem.
+                                <h1 class="max-w-2xl mb-4 text-2xl md:text-4xl xl:text-4xl font-extrabold leading-none tracking-tight text-[#003b99] ">AIR FREIGHT</h1>
+                                <p class="max-w-2xl mb-6 text-black lg:mb-8 text-sm  font-normal">
+                                Our air freight services are designed for speed and efficiency, ensuring timely deliveries to meet your urgent shipping needs. We work with a global network of air carriers to offer you competitive air freight rates without compromising on the quality of service. Our air freight team is highly skilled and experienced, offering you the optimal routing for your cargo based on your requirements. We provide end-to-end air freight solutions, including customs clearance and door-to-door delivery.
                                 </p>       
                             </div>
                              <div class=" md:flex col-span-6 md:col-span-4 gs_revealY">
-                                <img class="md:pl-24 py-10 md:py-0" src="<?php bloginfo('template_directory');?>/images/service/Asset 10.png" alt="HeroImage">              
+                                <img class="md:pl-24 py-10 md:py-0" src="<?php bloginfo('template_directory');?>/images/revisi/Asset 2@4x 1.png" alt="HeroImage">              
                             </div>    
                     </div>
                 </section>
 
-                <section class="bg-[#004179]" id="4" >    
+                <section class="bg-white" id="4" >    
                         
                        <div class="grid max-w-screen-xl px-8 mx-auto lg:gap-8 xl:gap-0 lg:pt-4 lg:grid-cols-12 ">
                         <div class="place-self-start lg:col-span-12 ">
@@ -125,12 +125,12 @@
 
                    <div class="grid max-w-screen-xl px-4 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:pb-16 pt-10 md:grid-cols-12 grid-cols-6">
                             <div class="md:flex col-span-6 md:col-span-4 gs_revealY">
-                                <img class="md:pl-24 py-10 md:py-0" src="<?php bloginfo('template_directory');?>/images/service/Asset 11.png" alt="HeroImage">              
+                                <img class="md:pl-24 py-10 md:py-0" src="<?php bloginfo('template_directory');?>/images/revisi/Asset 3@4x 1.png" alt="HeroImage">              
                             </div>
                             <div class="lg:mr-auto place-self-center col-span-6 md:col-span-7 md:px-24 px-4">
-                                <h1 class="max-w-2xl mb-4 text-2xl md:text-4xl xl:text-4xl font-bold leading-none tracking-tight text-[#fdce28] ">CROSS COUNTRY BOOKING</h1>
-                                <p class="max-w-2xl mb-6 text-white lg:mb-8 text-sm  font-normal">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In porta est sit amet condimentum ullamcorper. Pellentesque viverra pretium odio, fringilla aliquam elit bibendum in. Nulla pretium dolor id enim molestie vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus metus ut cursus eleifend. Praesent justo purus, tincidunt ut sem ut, accumsan cursus sem.
+                                <h1 class="max-w-2xl mb-4 text-2xl md:text-4xl xl:text-4xl font-extrabold leading-none tracking-tight text-[#fdce28] ">CROSS COUNTRY BOOKING</h1>
+                                <p class="max-w-2xl mb-6 text-black lg:mb-8 text-sm  font-normal">
+                            Cross Trade Shipments Made Easy. Our freight forwarding services are designed to make cross trade shipments easy and stress-free. With our global network of partners and our experience in global presence of logistics, we can help you move your goods from and to anywhere in the world with ease despite of your current location or home country.
                                 </p>
                                 
                             </div>
@@ -140,19 +140,19 @@
                     
                 </section>
 
-                <section class="bg-white" id="3">    
+                <section class="bg-[#003b99]" id="3">    
                         
                     <div class="grid max-w-screen-xl px-4 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:pb-16 pt-10 md:grid-cols-12 grid-cols-6">
                            
                          <div class="lg:mr-auto place-self-center col-span-6 md:col-span-7 md:px-24 px-4">
-                                <h1 class="max-w-2xl mb-4 text-2xl md:text-4xl xl:text-4xl font-bold leading-none tracking-tight text-sky-900 ">INTERMODAL & WAREHOUSING</h1>
-                                <p class="max-w-2xl mb-6 text-gray-700 lg:mb-8 text-sm  font-normal">
-                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. In porta est sit amet condimentum ullamcorper. Pellentesque viverra pretium odio, fringilla aliquam elit bibendum in. Nulla pretium dolor id enim molestie vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus metus ut cursus eleifend. Praesent justo purus, tincidunt ut sem ut, accumsan cursus sem.
+                                <h1 class="max-w-2xl mb-4 text-2xl md:text-4xl xl:text-4xl font-extrabold leading-none tracking-tight text-[#fdce28] ">INTERMODAL & WAREHOUSING</h1>
+                                <p class="max-w-2xl mb-6 text-white lg:mb-8 text-sm  font-normal">
+                            With our intermodal transportation service, we offer a seamless transportation solution that combines the strengths of different modes of transportation, including rail, trucking, and ocean shipping (domestic). This allows us to provide you with flexible, reliable, and cost-effective transportation options that can meet your unique needs while our warehousing service is also designed to meet your specific needs.
                                 </p>
                                 
                             </div>
                             <div class="md:flex col-span-6 md:col-span-4 gs_revealY">
-                                <img class="md:pl-24 py-10 md:py-0" src="<?php bloginfo('template_directory');?>/images/service/Asset 12.png" alt="HeroImage">              
+                                <img class="md:pl-24 py-10 md:py-0" src="<?php bloginfo('template_directory');?>/images/revisi/Asset 4@4x 1.png" alt="HeroImage">              
                             </div>
                                         
                         
